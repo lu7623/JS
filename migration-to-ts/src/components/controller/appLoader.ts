f18b440279770ff96b08ddca3634f6eff0f6d181
Lyubov Agulova <lu7623@gmail.com>
2023-06-12 14:46:12 +0300

    feat: responsive added

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '9c57e05e8c474bd383c2276313637128', // получите свой ключ https://newsapi.org/
         });
     }
