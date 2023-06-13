b7857afc93f5d4ff9975deac98f54d3deeeff0ad
Lyubov Agulova <lu7623@gmail.com>
2023-06-13 16:49:56 +0300

    init: start task

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+
+module.exports = {
+  entry: path.join(__dirname, 'RSS-CSS-Selectors/src/index'),
+  output: {
+    path: path.join(__dirname, 'RSS-CSS-Selectors/dist'),
+    filename: 'index.[contenthash].js',
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/i, use: 'ts-loader' },
+       {
+         test: /\.(scss|css)$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+       },
+       { 
+        test: /\.(png|jpg|jpeg|gif|sg)$/i,
+              type: 'asset/resource',
+              },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+},
+  plugins: [
+    new EslingPlugin({ extensions: 'ts' }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'RSS-CSS-Selectors/src/template.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+             filename: '[name].[contenthash].css',
+           }),
+     new FileManagerPlugin({
+                 events: {
+                 onStart: {
+                delete: ['RSS-CSS-Selectors/dist'],
+                  },
+                 },
+               }),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'RSS-CSS-Selectors/src'),
+    port: 9000,
+  },
+};
