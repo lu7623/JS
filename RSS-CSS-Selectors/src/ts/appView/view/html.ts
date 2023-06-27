e94d429a2366790ee3804eb2c6a958a22272dfe7
Lyubov Agulova <lu7623@gmail.com>
2023-06-27 10:14:02 +0300

    feat: data attributes added

--- RSS-CSS-Selectors/src/ts/appView/view/html.ts
+++ RSS-CSS-Selectors/src/ts/appView/view/html.ts
@@ -1,47 +1,53 @@
-import { TreeNode } from "../../model/levels";
-import { levelParams, levels } from "../../model/levels";
+import { TreeNode } from '../../model/levels';
+import { levelParams, levels } from '../../model/levels';
 
-
-
-export function node2Text (root:TreeNode, parentElem: HTMLElement):void {
-  const treeRoot = document.createElement('div');
-  const atr =  (root.attributes?.class ? ` class="${root.attributes?.class}"` : '') || (root.attributes?.id ? ` id="${root.attributes?.id}"` : '');
- if (!root.children) {treeRoot.innerText = `  <${root.tag}${atr} />`;}
- else {treeRoot.innerText = `<${root.tag}>`;}
-  if (root.attributes?.data) {
-      toolTipHandler(treeRoot, root)
+export function node2Text(root: TreeNode, parentElem: HTMLElement, lvl = 0): void {
+    const treeRoot = document.createElement('pre');
+    const atr =
+        (root.attributes?.class ? ` class="${root.attributes?.class}"` : '') ||
+        (root.attributes?.id ? ` id="${root.attributes?.id}"` : '');
+    if (!root.children) {
+        treeRoot.innerText = '  '.repeat(lvl) + `<${root.tag}${atr} />`;
+        lvl -= 1;
+    } else {
+        treeRoot.innerText = '  '.repeat(lvl) + `<${root.tag}${atr}>`;
+    }
+    if (root.attributes?.data) {
+        toolTipHandler(treeRoot, root);
+    }
+    parentElem.append(treeRoot);
+    if (root.children) {
+        lvl += 1;
+        root.children.forEach((child) => node2Text(child, treeRoot, lvl));
+        lvl -= 1;
+        const newtext = document.createTextNode('  '.repeat(lvl) + `</${root.tag}>`);
+        parentElem.appendChild(newtext);
+    }
 }
-  parentElem.append(treeRoot);
-  if (root.children) {root.children.forEach (child => node2Text(child, treeRoot));
-    const newtext = document.createTextNode(`</${root.tag}>`);
-    parentElem.appendChild(newtext);
-  }
-  
-  }
 
 export const htmlChange = (i: levels) => {
-  const htmlCode = document.querySelector('.html-code') as HTMLElement;
-  htmlCode?.replaceChildren();
-  if (htmlCode) node2Text(levelParams[i].node, htmlCode);
-}
+    const htmlCode = document.querySelector('.html-code') as HTMLElement;
+    htmlCode?.replaceChildren();
+    if (htmlCode) node2Text(levelParams[i].node, htmlCode);
+};
 
-
-export const toolTipHandler = (treeRoot: HTMLElement, root:TreeNode) => {
-  const tooltip = document.createElement('span');
-  tooltip.classList.add('tooltip');
-  tooltip.innerText = root.attributes?.data || '';
-  treeRoot.append(tooltip);
-  if (root.attributes?.unique) tooltip.setAttribute("data-unique", `${root.attributes.unique}`);
-  treeRoot.addEventListener('mouseover', (event) => {
-   event.stopPropagation();
-   treeRoot.classList.add('selected');
-   document.querySelectorAll(`.tooltip[data-unique="${root.attributes?.unique}"`).forEach(tip => 
-       {tip.classList.add('visible');
-       }) 
-  
-})
-treeRoot.addEventListener('mouseout', () => {
-document.querySelectorAll(`.tooltip[data-unique="${root.attributes?.unique}"`).forEach(tip => {tip.classList.remove('visible')
-   treeRoot.classList.remove('selected');}) 
-})
-}
+export const toolTipHandler = (treeRoot: HTMLElement, root: TreeNode) => {
+    const tooltip = document.createElement('span');
+    tooltip.classList.add('tooltip');
+    tooltip.innerText = root.attributes?.data || '';
+    treeRoot.append(tooltip);
+    if (root.attributes?.unique) tooltip.setAttribute('data-unique', `${root.attributes.unique}`);
+    treeRoot.addEventListener('mouseover', (event) => {
+        event.stopPropagation();
+        treeRoot.classList.add('selected');
+        document.querySelectorAll(`.tooltip[data-unique="${root.attributes?.unique}"`).forEach((tip) => {
+            tip.classList.add('visible');
+        });
+    });
+    treeRoot.addEventListener('mouseout', () => {
+        document.querySelectorAll(`.tooltip[data-unique="${root.attributes?.unique}"`).forEach((tip) => {
+            tip.classList.remove('visible');
+            treeRoot.classList.remove('selected');
+        });
+    });
+};
--- RSS-CSS-Selectors/src/ts/model/levels.ts
+++ RSS-CSS-Selectors/src/ts/model/levels.ts
@@ -1,4 +1,3 @@
-
 enum Tags {
     board = 'board',
     plate = 'plate',
@@ -8,23 +7,25 @@ enum Tags {
     sause = 'sause',
     wasabi = 'wasabi',
     sushi = 'sushi',
-    roll = 'roll'
+    roll = 'roll',
 }
 
 type Attributes = {
-    class?: string,
-    id?: string,
-    data?: string,
-    unique?: string
+    class?: string;
+    id?: string;
+    data?: string;
+    unique?: string;
+};
+export class TreeNode {
+    constructor(
+        readonly tag: Tags,
+        readonly isTarget: boolean = false,
+        readonly children?: TreeNode[],
+        readonly attributes?: Attributes
+    ) {}
 }
-export class TreeNode
-{    
-    constructor (readonly tag:Tags, readonly isTarget: boolean = false, readonly children?: TreeNode[], readonly attributes?: Attributes) {  }
-}
 
-
-export type levels = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 ;
-
+export type levels = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
 
 interface Task {
     level: levels;
@@ -38,74 +39,254 @@ export const levelParams: Task[] = [
         level: 0,
         answer: 'rice',
         description: 'Select all rice bouls',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.rice, true, undefined, {data: '<rice />', unique: 'rice1'}), new TreeNode(Tags.rice, true, undefined, {data: '<rice />', unique: 'rice2'})], {class: 'small'}),
+        node: new TreeNode(
+            Tags.board,
+            false,
+            [
+                new TreeNode(Tags.rice, true, undefined, { data: '<rice />', unique: 'rice1' }),
+                new TreeNode(Tags.rice, true, undefined, { data: '<rice />', unique: 'rice2' }),
+            ],
+            { class: 'small' }
+        ),
     },
     {
         level: 1,
         answer: '#spicy',
         description: 'Select spicy ramen',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.ramen, false, undefined, {data: '<ramen />', unique: 'ramen1'}), new TreeNode(Tags.ramen, true, undefined, {id: 'spicy', data: '<ramen id="spicy"/>',  unique: 'ramen2'})], {class: 'small'}),
+        node: new TreeNode(
+            Tags.board,
+            false,
+            [
+                new TreeNode(Tags.ramen, false, undefined, { data: '<ramen />', unique: 'ramen1' }),
+                new TreeNode(Tags.ramen, true, undefined, {
+                    id: 'spicy',
+                    data: '<ramen id="spicy"/>',
+                    unique: 'ramen2',
+                }),
+            ],
+            { class: 'small' }
+        ),
     },
     {
         level: 2,
         answer: 'plate sushi',
         description: 'Select sushi on the plate',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.plate, false, [new TreeNode(Tags.sushi, true, undefined, {data: '<sushi />',  unique: 'sushi0'})], {class: 'small', data: '<plate></plate>',  unique: 'plate'}), new TreeNode(Tags.sushi, false, undefined, {data: '<sushi />',  unique: 'sushi1'}),  new TreeNode(Tags.sushi, false, undefined, {data: '<sushi />',  unique: 'sushi2'})]),
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(
+                Tags.plate,
+                false,
+                [new TreeNode(Tags.sushi, true, undefined, { data: '<sushi />', unique: 'sushi0' })],
+                { class: 'small', data: '<plate></plate>', unique: 'plate' }
+            ),
+            new TreeNode(Tags.sushi, false, undefined, { data: '<sushi />', unique: 'sushi1' }),
+            new TreeNode(Tags.sushi, false, undefined, { data: '<sushi />', unique: 'sushi2' }),
+        ]),
     },
     {
         level: 3,
-        answer:  '.salmon',
+        answer: '.salmon',
         description: 'Select items with class "salmon"',
-        node: new TreeNode(Tags.board, false, [ new TreeNode(Tags.plate, false, [new TreeNode(Tags.roll, true, undefined, {class: 'salmon', data: '<roll class="salmon" />',  unique: 'roll1'}), new TreeNode(Tags.roll, true, undefined, {class: 'salmon',  data: '<roll class="salmon" />',  unique: 'roll2'})], { data: '<plate></plate>',  unique: 'plate'}),  new TreeNode(Tags.sushi, false, undefined, { data: '<sushi />',  unique: 'sushi1'}),  new TreeNode(Tags.sushi, true, undefined, {class: 'salmon',  data: '<sushi class="salmon" />',  unique: 'sushi2'})]),
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(
+                Tags.plate,
+                false,
+                [
+                    new TreeNode(Tags.roll, true, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon" />',
+                        unique: 'roll1',
+                    }),
+                    new TreeNode(Tags.roll, true, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon" />',
+                        unique: 'roll2',
+                    }),
+                ],
+                { data: '<plate></plate>', unique: 'plate' }
+            ),
+            new TreeNode(Tags.sushi, false, undefined, { data: '<sushi />', unique: 'sushi1' }),
+            new TreeNode(Tags.sushi, true, undefined, {
+                class: 'salmon',
+                data: '<sushi class="salmon" />',
+                unique: 'sushi2',
+            }),
+        ]),
     },
-  
+
     {
         level: 4,
         answer: 'wasabi , ginger',
         description: 'Select wasabi and ginger',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.wasabi, true, undefined, { data: '<wasabi />',  unique: 'wasabi'}),  new TreeNode(Tags.roll, false, undefined, { data: '<roll />',  unique: 'roll1'}), new TreeNode(Tags.roll, false, undefined, { data: '<roll />',  unique: 'roll2'} ), new TreeNode(Tags.ginger, true, undefined,  { data: '<ginger />',  unique: 'ginger'}), new TreeNode(Tags.sushi, false, undefined, {class: 'tuna',  data: '<sushi class="tuna" />',  unique: 'sushi1'}), new TreeNode(Tags.sushi, false, undefined, {class: 'tuna', data: '<sushi class="tuna" />',  unique: 'sushi2'})])
-        },
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(Tags.wasabi, true, undefined, { data: '<wasabi />', unique: 'wasabi' }),
+            new TreeNode(Tags.roll, false, undefined, { data: '<roll />', unique: 'roll1' }),
+            new TreeNode(Tags.roll, false, undefined, { data: '<roll />', unique: 'roll2' }),
+            new TreeNode(Tags.ginger, true, undefined, { data: '<ginger />', unique: 'ginger' }),
+            new TreeNode(Tags.sushi, false, undefined, {
+                class: 'tuna',
+                data: '<sushi class="tuna" />',
+                unique: 'sushi1',
+            }),
+            new TreeNode(Tags.sushi, false, undefined, {
+                class: 'tuna',
+                data: '<sushi class="tuna" />',
+                unique: 'sushi2',
+            }),
+        ]),
+    },
     {
         level: 5,
         answer: '*',
         description: 'Select all items on table',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.ginger, true, undefined, {data: 'ginger />',  unique: 'ginger'}),  new TreeNode(Tags.ramen, true, undefined, {data: '<ramen />',  unique: 'ramen'}), new TreeNode(Tags.sushi, true, undefined, {data: '<sushi />',  unique: 'sushi1'}),  new TreeNode(Tags.sushi, true, undefined, {data: '<sushi />',  unique: 'sushi2'}), new TreeNode(Tags.sause, true, undefined, {data: '<sause />',  unique: 'sause'})])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(Tags.ginger, true, undefined, { data: 'ginger />', unique: 'ginger' }),
+            new TreeNode(Tags.ramen, true, undefined, { data: '<ramen />', unique: 'ramen' }),
+            new TreeNode(Tags.sushi, true, undefined, { data: '<sushi />', unique: 'sushi1' }),
+            new TreeNode(Tags.sushi, true, undefined, { data: '<sushi />', unique: 'sushi2' }),
+            new TreeNode(Tags.sause, true, undefined, { data: '<sause />', unique: 'sause' }),
+        ]),
     },
     {
         level: 6,
-        answer:  'rice + sushi',
+        answer: 'rice + sushi',
         description: 'Select all sushi next to rice',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.sushi, false, undefined, {class: 'tuna'}), new TreeNode(Tags.rice, false),  new TreeNode(Tags.sushi, true, undefined, {class: 'unagi'}),  new TreeNode(Tags.rice, false), new TreeNode(Tags.sushi, true, undefined, {class: 'unagi'})])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(Tags.sushi, false, undefined, {
+                class: 'tuna',
+                data: '<sushi class="tuna"/>',
+                unique: 'sushi1',
+            }),
+            new TreeNode(Tags.rice, false, undefined, { data: '<rice />', unique: 'rice1' }),
+            new TreeNode(Tags.sushi, true, undefined, {
+                class: 'unagi',
+                data: '<sushi class="unagi" />',
+                unique: 'sushi2',
+            }),
+            new TreeNode(Tags.rice, false, undefined, { data: '<rice />', unique: 'rice2' }),
+            new TreeNode(Tags.sushi, true, undefined, {
+                class: 'unagi',
+                data: '<sushi class="unagi" />',
+                unique: 'sushi3',
+            }),
+        ]),
     },
-   
+
     {
         level: 7,
         answer: 'ramen ~ sushi',
         description: 'Select sushi to the right of ramen',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.sushi, false, undefined, {class: 'octopus'}), new TreeNode(Tags.ramen, false),  new TreeNode(Tags.sushi, true),  new TreeNode(Tags.sushi, true, undefined, {class: 'octopus'}),  new TreeNode(Tags.sushi, true)])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(Tags.sushi, false, undefined, {
+                class: 'octopus',
+                data: '<sushi class="octopus" />',
+                unique: 'sushi1',
+            }),
+            new TreeNode(Tags.ramen, false, undefined, { data: '<ramen />', unique: 'ramen' }),
+            new TreeNode(Tags.sushi, true, undefined, { data: '<sushi />', unique: 'sushi2' }),
+            new TreeNode(Tags.sushi, true, undefined, {
+                class: 'octopus',
+                data: '<sushi class="octopus" />',
+                unique: 'sushi3',
+            }),
+            new TreeNode(Tags.sushi, true, undefined, { data: '<sushi />', unique: 'sushi4' }),
+        ]),
     },
     {
         level: 8,
         answer: 'plate > roll',
         description: 'Select all rolls on the plate',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.wasabi, false), new TreeNode(Tags.plate, false, [new TreeNode(Tags.roll, true), new TreeNode(Tags.roll, true), new TreeNode(Tags.roll, true)]),  new TreeNode(Tags.sushi, false, undefined, {class: 'tuna'}),  new TreeNode(Tags.roll, false)])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(Tags.wasabi, false, undefined, { data: '<wasabi />', unique: 'sushi2' }),
+            new TreeNode(
+                Tags.plate,
+                false,
+                [
+                    new TreeNode(Tags.roll, true, undefined, { data: '<roll />', unique: 'roll1' }),
+                    new TreeNode(Tags.roll, true, undefined, { data: '<roll />', unique: 'roll2' }),
+                    new TreeNode(Tags.roll, true, undefined, { data: '<roll />', unique: 'roll3' }),
+                ],
+                { data: '<plate></plate>', unique: 'plate' }
+            ),
+            new TreeNode(Tags.sushi, false, undefined, {
+                class: 'tuna',
+                data: '<sushi class="tuna" />',
+                unique: 'sushi1',
+            }),
+            new TreeNode(Tags.roll, false, undefined, { data: '<roll />', unique: 'roll4' }),
+        ]),
     },
     {
         level: 9,
         answer: 'table:first-child',
         description: 'Select the 1st roll on plate',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.plate, false, [new TreeNode(Tags.roll, true, undefined, {class: 'salmon'}), new TreeNode(Tags.roll, false, undefined, {class: 'salmon'}), new TreeNode(Tags.roll, false, undefined, {class: 'salmon'})]),  new TreeNode(Tags.sushi, false), new TreeNode(Tags.roll, false, undefined, {class: 'salmon'})])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(
+                Tags.plate,
+                false,
+                [
+                    new TreeNode(Tags.roll, true, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon" />',
+                        unique: 'roll1',
+                    }),
+                    new TreeNode(Tags.roll, false, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon" />',
+                        unique: 'roll2',
+                    }),
+                    new TreeNode(Tags.roll, false, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon" />',
+                        unique: 'roll3',
+                    }),
+                ],
+                { data: '<plate></plate>', unique: 'plate' }
+            ),
+            new TreeNode(Tags.sushi, false, undefined, { data: '<sushi />', unique: 'sushi1' }),
+            new TreeNode(Tags.roll, false, undefined, {
+                class: 'salmon',
+                data: '<roll class="salmon" />',
+                unique: 'roll4',
+            }),
+        ]),
     },
     {
         level: 10,
         answer: 'roll:nth-of-type(even)',
         description: 'Select even rolls',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.roll, false), new TreeNode(Tags.roll, true), new TreeNode(Tags.roll, false), new TreeNode(Tags.roll, true),new TreeNode(Tags.roll, false), new TreeNode(Tags.roll, true)])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(Tags.roll, false, undefined, { data: '<roll />', unique: 'roll1' }),
+            new TreeNode(Tags.roll, true, undefined, { data: '<roll />', unique: 'roll2' }),
+            new TreeNode(Tags.roll, false, undefined, { data: '<roll />', unique: 'roll3' }),
+            new TreeNode(Tags.roll, true, undefined, { data: '<roll />', unique: 'roll4' }),
+            new TreeNode(Tags.roll, false, undefined, { data: '<roll />', unique: 'roll5' }),
+            new TreeNode(Tags.roll, true, undefined, { data: '<roll />', unique: 'roll6' }),
+        ]),
     },
     {
         level: 11,
         answer: 'plate:empty',
         description: 'Select an empty plate',
-        node: new TreeNode(Tags.board, false, [new TreeNode(Tags.plate, false, [new TreeNode(Tags.roll, false, undefined, {class: 'salmon'}), new TreeNode(Tags.roll, false, undefined, {class: 'salmon'})], {class: 'small'}),  new TreeNode(Tags.sause, false), new TreeNode(Tags.plate, true, undefined,  {class: 'small'})])
+        node: new TreeNode(Tags.board, false, [
+            new TreeNode(
+                Tags.plate,
+                false,
+                [
+                    new TreeNode(Tags.roll, false, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon"/>',
+                        unique: 'roll1',
+                    }),
+                    new TreeNode(Tags.roll, false, undefined, {
+                        class: 'salmon',
+                        data: '<roll class="salmon"/>',
+                        unique: 'roll2',
+                    }),
+                ],
+                { class: 'small', data: '<plate></plate>', unique: 'plate1' }
+            ),
+            new TreeNode(Tags.sause, false, undefined, { data: '<sause />', unique: 'sause' }),
+            new TreeNode(Tags.plate, true, undefined, { class: 'small', data: '<plate></plate>', unique: 'plate2' }),
+        ]),
     },
 ];
