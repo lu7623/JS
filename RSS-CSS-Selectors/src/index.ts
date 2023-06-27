265398e428fbd5b45059588d70f3856817c1b9cd
Lyubov Agulova <lu7623@gmail.com>
2023-06-27 11:52:21 +0300

    feat: simple input check added

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,9 +1,40 @@
 
-import { levelsDirectChange, viewOnLoad } from "./ts/controller/controller";
+import { viewNewLevel } from "./ts/appView/view";
+import { currentState, levelsDirectChange, viewOnLoad } from "./ts/controller/controller";
 import { setLocalStorage } from "./ts/controller/controller";
+import { levels } from "./ts/model/levels";
+import { levelParams } from "./ts/model/levels";
 
 
 viewOnLoad();
 levelsDirectChange();
 window.addEventListener("beforeunload", setLocalStorage);
 
+const input = document.getElementById('answer') as HTMLInputElement;
+
+input?.addEventListener( 'keydown', (event) => {
+    if (event.code === 'Enter'){
+    console.log(input.value);
+    if (input.value === levelParams[currentState.currentLevel].answer) onRightAnswer();
+    else onWrongAnswer();
+    input.value = '';
+   
+    }
+  });
+
+const onRightAnswer = () => {
+    if (currentState.currentLevel < 11){
+        currentState.userLevels.push(currentState.currentLevel)
+        currentState.currentLevel += 1;
+ 
+ viewNewLevel(currentState.currentLevel as levels);
+    }
+}
+
+const onWrongAnswer = () => {
+   document.getElementById('table-area')?.classList.add('wrong');  
+setTimeout(() => {
+    document.getElementById('table-area')?.classList.remove('wrong');  
+}, 1000);
+
+}
--- RSS-CSS-Selectors/src/ts/appView/view/css.ts
+++ RSS-CSS-Selectors/src/ts/appView/view/css.ts
@@ -1,11 +1,1 @@
-// function node2Text (root: TreeNode, lvl = 0):string {
-//     const atr =  (root.attributes?.class ? ` class="${root.attributes?.class}"` : '') || (root.attributes?.id ? ` id="${root.attributes?.id}"` : '');
-//      let str = `${' '.repeat(lvl)}<${root.tag}${atr}>\n`;
-//      if (root.children) {
-//         lvl +=1;
-//       str +=  root.children.map(child => node2Text(child, lvl)).join('');
-//       lvl -=1;
-//      }
-//      str += `${' '.repeat(lvl)}</${root.tag}>\n`
-//      return str;
-//     }
+
