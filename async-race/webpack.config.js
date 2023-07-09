a0c3f404eb96ac52d5c1cc98b4cf9eea62a9520f
Lyubov Agulova <lu7623@gmail.com>
2023-07-09 14:15:58 +0300

    init: start task

---
+++ async-race/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                             MiniCssExtractPlugin.loader,
+                                'css-loader',
+                                'postcss-loader',
+                                'sass-loader',
+                              ],
+            },
+            {
+                        test: /\.(png|jpg|jpeg|gif)$/i,
+                        type: 'asset/resource',
+                     },
+                     {
+                      test: /\.svg$/,
+                        type: 'asset/resource',
+                        generator: {
+                          filename: path.join('icons', '[name].[contenthash][ext]'),
+                      },
+                      },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), 
+            filename: 'index.html', 
+        }),
+        new CleanWebpackPlugin(),
+        new ESLintPlugin(),
+        new MiniCssExtractPlugin({
+               filename: '[name].[contenthash].css',
+             }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+         port: 9000,
+      },
+}
